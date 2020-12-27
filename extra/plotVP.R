95e8e057ad971af507d111114ddc29a0dfe2d80f
Ebrahimpoor <M.Ebrahimpoor@lumc.nl>
2020-12-27 15:44:47 +0100

    example

--- extra/plotVP.R
+++ extra/plotVP.R
@@ -3,23 +3,24 @@ load("Data/exData.RData")
 
 
 exData$selected<-0
-exData$selected[which(abs(exData$betahat)>1 & exData$pval.fdr<0.1)]<-1
+exData$selected[which(abs(exData$logFC)>2 & exData$adj.P.Val<0.05)]<-1
 
 
 #volcano plot
 ggplot() +
-     geom_point(data=exData,aes(x = betahat, y = -log10(pval),
-                                 color = factor(selected), shape=as.factor(8*isnull)))+
-     geom_hline(yintercept=-log10(max(exData$pval[exData$selected==1])),
-                 linetype="solid", color = "gray95", size=1)+
-     geom_vline(xintercept=1, linetype="solid", color = "gray95", size=1)+
-     geom_vline(xintercept=-1, linetype="solid", color = "gray95", size=1)+
+     geom_point(data=exData,aes(x = logFC, y = -log10(P.Value),
+                                 color = factor(selected),
+                                shape=truth),size=2)+
+     geom_hline(yintercept=-log10(max(exData$P.Value[exData$selected==1])),
+                 linetype="solid", color = "black", size=1)+
+     geom_vline(xintercept=2, linetype="solid", color = "black", size=1)+
+     geom_vline(xintercept=-2, linetype="solid", color = "black", size=1)+
      scale_colour_manual(name = "",
                         labels = c("Selected" ,"Not Selected"),
                         values = c("red", "blue"),
                         limits = c("1", "0")) +
      scale_shape_manual(name = "",labels = c("Null" ,"non-Null"),
-                         values = c(16, 17)) +
+                         values = c(17, 16)) +
      xlab("Fold Change") +
      ylab("-Log10 p-value")+
      theme_minimal()+
